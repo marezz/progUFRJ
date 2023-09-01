d0d4c6a06d5f9fc61ef3f8058765cedba7c88564
marezz <marcelob@dcc.ufrj.br>
2023-09-01 08:56:56 -0300

    Changes to be committed: 	modified:   aula2_01.09/ex2.c 	new file:   aula2_01.09/output/ex2

--- aula2_01.09/ex2.c
+++ aula2_01.09/ex2.c
@@ -1,1 +1,27 @@
-#
+#include <stdio.h>
+
+
+typedef struct{
+    int minuto, hora;
+} horario;
+
+void conversao(int min, horario *dia){
+
+    (*dia).hora = min/60;
+    (*dia).minuto = min - (*dia).hora*60;
+
+}
+
+int main(){
+
+    int minutos;
+    horario dia;
+
+    printf("Insira os minutos: ");
+    scanf("%d", &minutos);
+
+    conversao(minutos, &dia);
+    printf("%d horas e %d minutos\n", dia.hora, dia.minuto);
+
+    return 0;
+}
